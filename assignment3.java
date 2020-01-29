9d2ee9722ea90e709f249d833d5eedc2cda8b5e9
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-01-29 13:34:30 -0800

    Updates the loop

--- assignment3.java
+++ assignment3.java
@@ -1,29 +1,26 @@
-import java.lang.Math;
 
 public class assignment3 {
-	//This code will print list of base numbers to the power of the exponent in ascending order
+	// This code will print list of base numbers to the power of the exponent in
+	// ascending order
 	public static void main(String[] args) {
-		// Calling the methods --code testing--
+		// testing the codes by calling method
 		PrintPowersOfN(5, 3);
 		PrintPowersOfN(2, 7);
 		PrintPowersOfN(-2, 8);
 		PrintPowersOfN(15, 3);
 	}
 
-	public static void PrintPowersOfN(int base, int exponent) { //this method has 2 parameters to be passed --base & exponent)
-		int result; //initialize the result variable
+	public static void PrintPowersOfN(int base, int exponent) { //this method has 2 parameters to be passed base & exponent)													
+		//initialize the result variable
 		System.out.print("List of " + base + " to the power of " + exponent + " are: ");
-		for (int i = 0; i <= exponent; i++) { //for loop to increment the exponent 
-			result = (int) Math.pow(base, i);
-			System.out.print(result + " ");
+		int result = 1;
+		System.out.print(result + " "); //printing the default result of x^0 = 1
+		
+		//looping the power by incrementing the exponent
+		for (int i = 1; i <= exponent; i++) {
+			result = base * result;
+			System.out.print(result + " "); //printing the result of each increment
 		}
-		System.out.println("");
-
-		/*
-		 * THIS PIECE OF CODE WILL START AT X^1 INSTEAD OF X^0
-		 * 
-		 * int result = 1; for (int i = 0; i <= exponent; i++) { result = base * result;
-		 * System.out.print(result + " "); } System.out.println();
-		 */
+		System.out.println(); //print the outcome of each methods
 	}
 }
